e58cd3da64d4b4cb2e8b9a62c09f08385b2ebb4d
Werneck <werneckcolins@outlook.com>
2023-10-23 20:27:41 -0300

    Criado projeto firebase

---
+++ js/config-firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbdsgeDLaTXZRNWNfBrD9VMH6x5QS6qNE",
+  authDomain: "projetotarefa-6f0f0.firebaseapp.com",
+  projectId: "projetotarefa-6f0f0",
+  storageBucket: "projetotarefa-6f0f0.appspot.com",
+  messagingSenderId: "341818565899",
+  appId: "1:341818565899:web:466430c2c81339584f2e0e"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
