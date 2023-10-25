a419a0e4d0fd1dbd6a5f22511f4db9d4c9fcc537
Werneck <werneckcolins@outlook.com>
2023-10-25 20:38:38 -0300

    Consultado dados

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 import {app, db} from "./config-firebase.js"
-import {doc, setDoc, collection, addDoc} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
+import {doc, setDoc, collection, addDoc, query, where, getDocs, orderBy} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js"
 
 let nome = document.querySelector("#tarefa")
 let data = document.querySelector("#data")
 let status = document.querySelector("#status")
 let btnTarefa = document.querySelector("#btnTarefa")
+let bloco = document.querySelector("#bloco")
 
 async function inserirTarefa(){
     try {
@@ -20,8 +21,35 @@ async function inserirTarefa(){
     }
 }
 
+async function consultarTarefa(){
+    const busca = query(collection(db, "tarefa"), orderBy("name"));
+
+    const resultado = await getDocs(busca);
+            resultado.forEach((item) => {
+  // item.data() is never undefined for query item snapshots
+    console.log(item.id, " => ", item.data());
+
+    bloco.innerHTML += `
+        <li class="list-group-item d-flex justify-content-between align-items-center mb-2">
+            <div class="ms-2 me-auto">
+            <strong>Nome: </strong> ${item.data().name} <br>
+            <strong>Data: </strong> ${item.data().data} <br>
+            <strong>Status: </strong> ${item.data().status} <br>
+            </div>
+
+            <div class="d-flex gap-2 justify-content-end">
+            <button type="button" class="btn btn-danger" id="excluir">Excluir</button>
+            <button type="button" class="btn btn-info" id="alterar">Alterar</button>
+            </div>
+        </li>
+    `
+
+});
+}
+
 btnTarefa.addEventListener("click", (evento)=>{
     evento.preventDefault()
     console.log(nome.value, data.value, status.value)
     inserirTarefa()
+    consultarTarefa()
 })
